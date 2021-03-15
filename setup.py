3af683db74adfb949cf6b53c936a13d8296f3f87
kushaswani <kushaswani@gmail.com>
2021-03-15 04:48:06 -0400

    added efficientdet prefix to files and folders

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open('requirements.txt', 'r') as fh:
 setup(
     name='Yet-Another-EfficientDet-Pytorch',
     version='0.1.0',
-    py_modules=['backbone'],
-    packages=find_packages(include=['efficientdet', 'efficientdet.*', 'efficientnet', 'efficientnet.*', 'backbone', 'utils', 'utils.*']),
+    py_modules=['efficientdet_backbone'],
+    packages=find_packages(include=['efficientdet', 'efficientdet.*', 'efficientnet', 'efficientnet.*', 'efficientdet_backbone', 'efficientdet_utils', 'efficientdet_utils.*']),
     url='https://github.com/kush-litterati/EfficientDet',
     license='',
     author='zylo117',
